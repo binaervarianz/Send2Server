6212308f055628b03d3194c09099874dc3eeb058
RoXXoR <roxxor@binaervarianz.de>
2010-09-25 21:53:13 +0200

    added AlertDialog which shows the URL to send

--- src/de/binaervarianz/sendtowebdav/SendToServer.java
+++ src/de/binaervarianz/sendtowebdav/SendToServer.java
@@ -1,13 +1,42 @@
 package de.binaervarianz.sendtowebdav;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 
 public class SendToServer extends Activity {
-    /** Called when the activity is first created. */
+
+	
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        
+        Intent intent = getIntent();
+        Bundle extras = intent.getExtras();
+        
+        if(intent.getAction().equals( Intent.ACTION_SEND ) && extras != null) {
+        	String url = "";
+        	if (extras.containsKey(Intent.EXTRA_TEXT)) {
+        		url += extras.getString(Intent.EXTRA_TEXT);
+        	}
+        		
+        AlertDialog.Builder alertDialogBuilder = new  AlertDialog.Builder(this);
+        alertDialogBuilder.setMessage(url.toString())
+        		   .setCancelable(false)
+        		   .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+        			   public void onClick(DialogInterface dialog, int id) {
+        				   dialog.cancel();
+        			   }
+        		   })
+        .setNegativeButton("No", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                 dialog.cancel();
+            }
+        });
+        AlertDialog alertDialog = alertDialogBuilder.create();
+        alertDialog.show();
+        }
     }
 }

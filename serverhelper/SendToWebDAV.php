3fedbb89cda67f781c6936b5a862fd8f0ee9569b
RoXXoR <roxxor@binaervarianz.de>
2011-01-09 16:40:05 +0100

    serverhelper script now supports binary files in subfolders like Images

--- serverhelper/SendToWebDAV.php
+++ serverhelper/SendToWebDAV.php
@@ -10,38 +10,55 @@
 <?php 
 $maxstrlen = 50;
 $filepath = '/webdav/';
+$protocol = 'https://';
+$server = $protocol.$_SERVER['HTTP_HOST'];
 $files = array();
 $folders = array();
 if ($handle = opendir('.'.$filepath)) {
     while (false !== ($file = readdir($handle))) {
-        if (preg_match('/URL.*\.txt/', $file)) {
-						$files[] = $file;
-				} else if (preg_match('/^Image-\d{14}$/', $file)) {
+        if (preg_match('/(URL|Image)-(\d{14}).*/', $file)) {
+						$files[$file] = preg_replace('/.+-(\d{14}).*/', '$1', $file);
+				} /*else if (preg_match('/^Image-\d{14}$/', $file)) {
 						$folders[] = $file;
-				}
+				}*/
 		}
 		closedir($handle);
 }
-sort($files);
-foreach ($files as $file) {
+
+arsort($files);
+foreach (array_keys($files) as $file) {
 	echo "\n".'<item>'."\n";
 	$match = '';
 	$link = '';
 	$title = '';
 	$date = array();
-	$content = file_get_contents(".$filepath".$file);
-	if (preg_match('/^[a-zA-Z]+[:\/\/]+[A-Za-z0-9\-_]+\\.+[A-Za-z0-9\.\/%&=\?\-_]+$/i', $content, $match)) {
-		$title = $match[0];
-		$link  = $match[0];
+
+	// 
+	if (is_dir(".$filepath".$file)) {
+		if ($handle = opendir(".$filepath".$file)) {	
+			while (false !== ($binfile = readdir($handle))) {
+				if (!preg_match('/^(\.|\.\.)$/', $binfile)) {
+					$title = $binfile;
+					$link = $server.$filepath.$file."/".$binfile."\n";
+					break;
+				}
+			}
+		}
 	} else {
-		$lines = preg_split('/\n/', $content);
-		$title = (strlen($lines[0]) < $maxstrlen) ? $lines[0] : substr($lines[0],0 ,$maxstrlen);
-		$link = 'https://'.$_SERVER['HTTP_HOST'].$filepath.$file;
+		$content = file_get_contents(".$filepath".$file);
+		if (preg_match('/^[a-zA-Z]+[:\/\/]+[A-Za-z0-9\-_]+\\.+[A-Za-z0-9\.\/%&=\?\-_]+$/i', $content, $match)) {
+			$title = $match[0];
+			$link  = $match[0];
+		} else {
+			$lines = preg_split('/\n/', $content);
+			$title = (strlen($lines[0]) < $maxstrlen) ? $lines[0] : substr($lines[0],0 ,$maxstrlen);
+			$link = $server.$filepath.$file;
+		}
 	} 
-	preg_match('/URL-(\d{4})(\d{2})(\d{2})(\d{2})(\d{2})(\d{2})/', $file, $date);
+	preg_match('/^(\d{4})(\d{2})(\d{2})(\d{2})(\d{2})(\d{2})$/', $files[$file], $date);
 	echo '<title>'.htmlspecialchars(trim($title)).'</title>'."\n";
-  echo '<link>'.htmlspecialchars($link).'</link>'."\n";
-	echo '<gid>'.substr($file,0,-4).'</gid>'."\n";
+  echo '<link>'.htmlspecialchars(trim($link)).'</link>'."\n";
+	echo '<gid>'.preg_replace('/(.+-\d{14}).*/', '$1', $file).'</gid>'."\n";
 	echo '<pubDate>'.date('D, d M Y H:i:s O', mktime($date[4], $date[5], $date[6], $date[2], $date[3], $date[1])).'</pubDate>'."\n";
 	echo '</item>'."\n";
 }

106742515d16119eaf131b310c6e69c3744d8cb2
Chaos <null.nospam.com>
2011-01-09 19:57:13 +0100

    threaded uploading, filesystem sends, correct activity stop
    - Upload of binary files is now done in a thread while showing a 'in progress' dialog
    - exception messages are communicated back to the main thread to be displayed as toasts (only for sending thread so far)
    - sharing of images from applications addressing them through their filesystem path is now possible (e.g. file expert)
    - the activity is now stopped explicitly after upload so the prior application can continue (no more frozen gallery screen)
    
    - Bugs: send activity gets restarted in mid-process if orientation is changed (see wiki)
    
    - ToDo: fix remaining toasts from within threads
    -       check for filesize, give real progress indication, warn for big files when not on wifi
    -       maybe make the uploading a background thread?

--- src/de/binaervarianz/sendtowebdav/SendToServer.java
+++ src/de/binaervarianz/sendtowebdav/SendToServer.java
@@ -9,13 +9,17 @@ import java.util.Date;
 import org.apache.http.HttpException;
 import org.apache.http.client.ClientProtocolException;
 
+
 import android.app.Activity;
+import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.provider.MediaStore;
 import android.util.Log;
 import android.widget.Toast;
@@ -24,10 +28,13 @@ public class SendToServer extends Activity {
 	private final String TAG = this.getClass().getName();
 	
 	private WebDAVhandler httpHandler;
+	private ProgressDialog dialog;
+	private String name, filePath, type;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		Log.d(TAG, "Activity started");
 
 		// read preferences
 		SharedPreferences settings = getSharedPreferences(ConfigWebDAV.PREFS_PRIVATE, Context.MODE_PRIVATE);
@@ -56,9 +63,9 @@ public class SendToServer extends Activity {
 		//figure out what's to be send
 		
 			//this is for debugging and getting information on new media types
-			String type = intent.resolveType(this);
+			this.type = intent.resolveType(this);
 			//URLS give text/plain, images image/jpeg ....
-			Log.d(TAG, type);
+			Log.d(TAG, this.type);
 			for (String s : extras.keySet()) {
 				Log.d(TAG, s);
 				//Urls give EXTRA_TEXT, images EXTRA_STREAM
@@ -71,56 +78,69 @@ public class SendToServer extends Activity {
 			
 			String url = "";
 			SimpleDateFormat dateformater = new SimpleDateFormat("yyyyMMddHHmmss"); 
-			try {				
-				if (extras.containsKey(Intent.EXTRA_TEXT)) {	// simple text like URLs
-					url += extras.getString(Intent.EXTRA_TEXT);
-					httpHandler.putFile("URL-"+dateformater.format(new Date())+".txt", "", url);
-					
-				} else if (extras.containsKey(Intent.EXTRA_STREAM)) {	// binary files
-					Uri contentUri = (Uri) extras.get(Intent.EXTRA_STREAM);
-					
-					//debug
-					Log.d(TAG, contentUri.toString());
-					
-					String filePath = "";
-					// there are real file system paths and logical content URIs
-					// I've so far only tested the first kind [chaos]
-					if (contentUri.toString().startsWith("content:")) {
-						filePath = this.getRealPathFromURI(contentUri);
-					} else {
-						filePath = contentUri.getEncodedPath();
-					}
-					// create a basename out of the type identifier
-					String basename = type.toUpperCase().charAt(0) + type.substring(1, type.indexOf('/'));
-					String name = basename + "-" + dateformater.format(new Date());
-					
-					//debug
-					Log.d(TAG, name);
-
-					httpHandler.putBinFile( name, "", filePath, type);
-				}
-			} catch (ClientProtocolException e) {
-				Toast.makeText(this, this.getString(R.string.app_name) + ": ClientProtocolException: "+e, Toast.LENGTH_LONG).show();
-				Log.e(TAG, "ClientProtocolException: "+e);
-				return;
-			} catch (IOException e) {
-				Toast.makeText(this, this.getString(R.string.app_name) + ": IOException: "+e, Toast.LENGTH_LONG).show();
-				Log.e(TAG, "IOException: "+e);
-				return;
-			} catch (IllegalArgumentException e) {
-				Toast.makeText(this, this.getString(R.string.app_name) + ": IllegalArgumentException: "+e, Toast.LENGTH_LONG).show();
-				Log.e(TAG, "IllegalArgumentException: "+e);
-				return;
-			} catch (HttpException e) {
-				Toast.makeText(this, this.getString(R.string.app_name) + ": HttpException: "+e, Toast.LENGTH_LONG).show();
-				Log.e(TAG, "HttpException: "+e);
-				return;
-			}
 			
-			Toast.makeText(this, R.string.data_send, Toast.LENGTH_SHORT).show();
+							
+			if (extras.containsKey(Intent.EXTRA_TEXT)) {	// simple text like URLs
+				url += extras.getString(Intent.EXTRA_TEXT);
+				
+				try {
+					// sending an URL is fast, so do it here
+					httpHandler.putFile("URL-"+dateformater.format(new Date())+".txt", "", url);
+				} catch (ClientProtocolException e) {
+					Toast.makeText(this, this.getString(R.string.app_name) + ": ClientProtocolException: "+e, Toast.LENGTH_LONG).show();
+					Log.e(TAG, "ClientProtocolException: "+e);
+					return;
+				} catch (IOException e) {
+					Toast.makeText(this, this.getString(R.string.app_name) + ": IOException: "+e, Toast.LENGTH_LONG).show();
+					Log.e(TAG, "IOException: "+e);
+					return;
+				} catch (IllegalArgumentException e) {
+					Toast.makeText(this, this.getString(R.string.app_name) + ": IllegalArgumentException: "+e, Toast.LENGTH_LONG).show();
+					Log.e(TAG, "IllegalArgumentException: "+e);
+					return;
+				} catch (HttpException e) {
+					Toast.makeText(this, this.getString(R.string.app_name) + ": HttpException: "+e, Toast.LENGTH_LONG).show();
+					Log.e(TAG, "HttpException: "+e);
+					return;
+				}
+				
+				// just a small message that the URL was send
+				Toast.makeText(this, R.string.data_send, Toast.LENGTH_SHORT).show();
+				this.finish();
+				
+			} else if (extras.containsKey(Intent.EXTRA_STREAM)) {	// binary files
+				Uri contentUri = (Uri) extras.get(Intent.EXTRA_STREAM);
+				
+				//debug
+				Log.d(TAG, contentUri.toString());
+				
+				
+				// there are real file system paths and logical content URIs
+				// I've so far only tested the first kind [chaos]
+				if (contentUri.toString().startsWith("content:")) {
+					this.filePath = this.getRealPathFromURI(contentUri);
+					Log.d(TAG,"path: "+this.filePath);
+				} else if (contentUri.toString().startsWith("file:")){
+					this.filePath = contentUri.getPath();
+					Log.d(TAG,"path: "+this.filePath);
+				}
+				
+				// create a basename out of the type identifier
+				String basename = type.toUpperCase().charAt(0) + type.substring(1, type.indexOf('/'));
+				this.name = basename + "-" + dateformater.format(new Date());
+				
+				//debug
+				Log.d(TAG, name);
+				
+				// sending files may take time, so do it in another thread and give a progress dialog
+				dialog = ProgressDialog.show(SendToServer.this, "", SendToServer.this.getString(R.string.sending), true);
+				new SendThread(handler).start();
+				
+			}	
 		}
-		
+		Log.d(TAG, "Activity closed");
 	}
+	
 	/**
 	 * convert a <content:> URI (like from the gallery) to an absolute path
 	 * 
@@ -135,4 +155,64 @@ public class SendToServer extends Activity {
         cursor.moveToFirst();
         return cursor.getString(column_index);
     }
+	
+	// Define the Handler that receives messages from the thread and update the progress
+    final Handler handler = new Handler() {
+        public void handleMessage(Message msg) {
+        	dialog.dismiss();
+        	Bundle data = msg.peekData();
+        	if (data != null) {
+            	Toast.makeText(SendToServer.this, getString(R.string.failure) +": /n" + data.getString("exception") , Toast.LENGTH_LONG).show();
+        	}
+        	SendToServer.this.finish();
+        }
+    };
+    
+	private class SendThread extends Thread {
+        Handler mHandler;
+       
+        SendThread(Handler h) {
+            mHandler = h;
+        }
+       
+        public void run() {
+        	try {
+        		httpHandler.putBinFile( name, "", filePath, type);        	
+			} catch (ClientProtocolException e) {				
+				Log.e(TAG, "ClientProtocolException: "+e);
+				Message m = Message.obtain();
+				Bundle b = new Bundle(1);
+				b.putString("exception", e.toString());
+				m.setData(b);
+				mHandler.sendMessage(m);
+				return;
+			} catch (IOException e) {
+				Log.e(TAG, "IOException: "+e);
+				Message m = Message.obtain();
+				Bundle b = new Bundle(1);
+				b.putString("exception", e.toString());
+				m.setData(b);
+				mHandler.sendMessage(m);
+				return;
+			} catch (IllegalArgumentException e) {
+				Log.e(TAG, "IllegalArgumentException: "+e);
+				Message m = Message.obtain();
+				Bundle b = new Bundle(1);
+				b.putString("exception", e.toString());
+				m.setData(b);
+				mHandler.sendMessage(m);
+				return;
+			} catch (HttpException e) {
+				Log.e(TAG, "HttpException: "+e);
+				Message m = Message.obtain();
+				Bundle b = new Bundle(1);
+				b.putString("exception", e.toString());
+				m.setData(b);
+				mHandler.sendMessage(m);
+				return;
+			}
+        	mHandler.sendEmptyMessage(0);
+        }
+    }
+	
 }
